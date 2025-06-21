42a334d5c3708bc72828f69c1cc544534c54f3f5
rama210989 <abhinav.ramamurthy_HUF@yahoo.com>
2025-06-21 09:34:27 +0530

    Update test_scraper.py

--- test_scraper.py
+++ test_scraper.py
@@ -1,9 +1,1 @@
-from fbref_scraper import fetch_team_player_stats
 
-# Example: Man City 2023–24 team stats page
-team_url = "https://fbref.com/en/squads/b8fd03ef/Manchester-City-Stats"
-df = fetch_team_player_stats(team_url)
-
-# Print key info
-print("Columns:", df.columns.tolist())
-print(df[['Player', 'Pos', 'Min']].head())
